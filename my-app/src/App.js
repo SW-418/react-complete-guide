18c530610f7c7d3bc3084153843b1ee7fefc526b
Sam Wells <samwells95@gmail.com>
2019-02-24 17:04:00 +0000

    add first react component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> My name Jeff</h1>
+        <h1>SW</h1>
+        <p>______</p>
+        <Person />
       </div>
     );
   }
---
+++ my-app/src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const person = () => {
+    return <p>Hey I'm walking here!</p>
+}
+
+export default person;
