1231487503860c3dab89d0f1e2edf31f0d02e6b0
Sam Wells <samwells95@gmail.com>
2019-02-24 17:31:37 +0000

    modify data using props and state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,34 @@ import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
+  //State only available in classes that extend Component!
+  state = {
+    people: [
+      { name: 'Sam', age: 24 },
+      { name: 'Drake', age: 69 },
+      { name: 'Frank', age: 42 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      people: [
+        { name: 'Sammy', age: 24 },
+        { name: 'Drake', age: 1069 },
+        { name: 'Frank', age: 420 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>SW</h1>
         <p>______</p>
-        <Person />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age}>My Hobbies: Getting Lit</Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} />
+        <Person name={this.state.people[2].name} age={this.state.people[2].age} />
       </div>
     );
   }
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const person = () => {
-    return <p>Hey I'm walking here and I am {Math.floor(Math.random() * 30)} years old!</p>
+const person = (props) => {
+    return (
+    <div>
+        <p>Hey my name is {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+    </div>
+    
+    )
 }
 
 export default person;
